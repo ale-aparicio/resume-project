2f57ef70a546da4d0d41a25c468c7e801beb2e5e
ale-aparicio <ale.aparicio@live.com>
2021-05-07 19:14:01 +0000

    add

--- resume-project/assets/js/github-information.js
+++ resume-project/assets/js/github-information.js
@@ -65,10 +65,10 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(userInformationHTML(userData));
                 $("#gh-repo-data").html(repoInformationHTML(repoData));
             }, function(errorResponse){
-                if (errorResponse.status === 404){
+                if (errorResponse.status === 403){
                     $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
                 } else if(errorResponse.status === 403){
-                    var resetTime = new Date(errorResponseHeader("X-RateLimit-Reset")* 1000);
+                    var resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset")*1000);
                     $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()} </h4>`)
                 } else {
                     console.log(errorResponse);
