04dc8581a35e41ff3c6335b643704ced66e793d9
ale-aparicio <ale.aparicio@live.com>
2021-05-07 17:53:50 +0000

    add

--- resume-project/assets/js/github-information.js
+++ resume-project/assets/js/github-information.js
@@ -1,7 +1,7 @@
-function fetchGithubInformation(event){
+function fetchGitHubInformation(event){
 
     var username = $("#gh-username").val();
-    if(!username) {
+    if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a Github username</h2>`);
         return;
     }
