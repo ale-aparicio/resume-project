ef63f96363cbb0f5962f3fc178206fb67f9fd90b
ale-aparicio <ale.aparicio@live.com>
2021-05-07 20:00:31 +0000

    add

--- resume-project/assets/js/sendEmail.js
+++ resume-project/assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    email.js.send("gmail", "rosie", {
+        "from_name" : contactForm.name.value,
+        "from_name" : contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response){
+            console.log("SUCESS", response);
+        }, function(error){
+            console.log("FAILED", error);
+        });
+
+        return false;
+}
